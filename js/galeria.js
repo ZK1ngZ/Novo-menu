688d5c2a2e482a2da06a2e797c7dfbaf2c508b35
Bruno Tavares <brunotavaresguimaraes2@gmail.com>
2023-05-30 15:09:36 -0300

    contatos arrumado

--- js/galeria.js
+++
@@ -1,8 +0,0 @@
-let btnMenu = document.querySelector("#btn-menu");
-
-btnMenu.addEventListener('click',() => {
-    let menu = document.querySelector("#nav-menu");
-    menu.classList.toggle('visivel');
-    btnMenu.classList.toggle('fa-xmark');
-}
-);
