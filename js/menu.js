5902235b4bd03e00e72e1961452d368e52870239
Bruno Tavares <brunotavaresguimaraes2@gmail.com>
2023-06-06 15:31:08 -0300

    btn-qtd

--- js/menu.js
+++ js/menu.js
@@ -11,3 +11,8 @@ document.documentElement.onclick = function(event) {
         document.documentElement.classList.remove('menu-ativo');
     }
 };
+setInterval( function atualizarPreco(){
+    var preco = document.getElementById('preco');
+    var qtd = document.getElementById('qtdn').value;
+    preco.innerHTML = 'por R$'+ qtd*330.00 + ',00 Ou 6x de R$' + (qtd*330)/6 + ' sem juros'
+}, 1000)
